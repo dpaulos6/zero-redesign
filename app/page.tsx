d835782afd94d556c5f6059b0a03ed9566972904
Diogo Paulos <itzframepvp@outlook.com>
2025-04-27 12:55:28 +0100

    feat: pixelated bg & half responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 "use client"
 
+import {
+  PixelatedBackground,
+  PixelatedLeft,
+  PixelatedRight
+} from "@/components/pixelated-bg"
 import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
@@ -45,9 +50,13 @@ export default function Home() {
   }, [handleTabChange])
 
   return (
-    <main className="relative flex flex-1 flex-col">
-      <header className="fixed z-50 flex w-full items-center justify-center pt-6">
-        <nav className="flex w-full max-w-2xl items-center justify-between gap-2 rounded-xl border bg-popover p-2">
+    <main className="relative flex flex-1 flex-col overflow-x-hidden">
+      <PixelatedBackground
+        className="-z-10 -translate-x-1/2 absolute top-0 left-1/2 h-auto w-screen min-w-[1920px] object-cover opacity-5"
+        style={{ mixBlendMode: "screen" }}
+      />
+      <header className="fixed z-50 flex w-full items-center justify-center px-4 pt-6">
+        <nav className="flex w-full max-w-3xl items-center justify-between gap-2 rounded-xl border border-input/50 bg-popover p-2">
           <div className="flex items-center gap-6">
             <div className="size-10 rounded-md bg-white" />
             <NavigationMenu>
@@ -86,8 +95,8 @@ export default function Home() {
           </div>
         </nav>
       </header>
-      <section className="mt-40 flex flex-col items-center">
-        <div className="mb-6 inline-flex items-center rounded-full border bg-[#2a2a2a] px-4 py-1.5 pr-1.5">
+      <section className="mt-40 flex flex-col items-center px-4">
+        <div className="mb-6 inline-flex items-center rounded-full border border-input/50 bg-[#2a2a2a] px-4 py-1.5 pr-1.5">
           <span>See what's new from O.email</span>
           <Link
             href="/#"
@@ -111,59 +120,64 @@ export default function Home() {
           Get Started
         </Button>
       </section>
-      <section className="mt-10 flex flex-col">
-        <div className="relative flex justify-center">
-          <div className="-translate-x-1/2 container absolute top-0 left-1/2 h-px bg-border xl:max-w-7xl" />
-          <Tabs
-            defaultValue="smart-categorization"
-            onValueChange={handleTabChange}
-            className="flex w-full items-center gap-0"
-          >
-            <div className="tabs-container relative flex w-full justify-center">
-              <TabsList className="relative h-fit rounded-none bg-transparent pb-0">
-                <div className="-top-4 absolute left-0 h-[calc(100%+16px)] w-px bg-border" />
-                <div className="-top-4 absolute right-0 h-[calc(100%+16px)] w-px bg-border" />
-                {/* Glow */}
-                <CustomTabGlow glowStyle={glowStyle} />
+      <section className="relative mt-10 flex flex-col justify-center">
+        <div className="-translate-x-1/2 absolute top-0 left-1/2 h-px w-full bg-border md:container xl:max-w-7xl" />
+        <Tabs
+          defaultValue="smart-categorization"
+          onValueChange={handleTabChange}
+          className="flex w-full items-center gap-0"
+        >
+          <div className="tabs-container relative flex w-full max-w-[40rem] justify-center md:max-w-max">
+            <TabsList className="relative h-fit w-full rounded-none bg-transparent pb-0 md:w-auto">
+              <div className="-top-4 absolute left-0 h-[calc(100%+16px)] w-px bg-border" />
+              <div className="-top-4 absolute right-0 h-[calc(100%+16px)] w-px bg-border" />
+              {/* Glow */}
+              <CustomTabGlow glowStyle={glowStyle} />
 
-                {/* Tab Triggers */}
-                {tabs.map((tab, index) => (
-                  <TabsTrigger
-                    key={tab.value}
-                    value={tab.value}
-                    className="!bg-transparent !shadow-none relative h-12 w-52 rounded-none"
-                    ref={(el) => {
-                      if (el) tabRefs.current[index] = el
-                    }}
-                  >
-                    {tab.label}
-                  </TabsTrigger>
-                ))}
-              </TabsList>
+              {/* Tab Triggers */}
+              {tabs.map((tab, index) => (
+                <TabsTrigger
+                  key={tab.value}
+                  value={tab.value}
+                  className="!bg-transparent !shadow-none relative h-12 w-52 rounded-none"
+                  ref={(el) => {
+                    if (el) tabRefs.current[index] = el
+                  }}
+                >
+                  {tab.label}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </div>
+          <div className="relative flex w-full justify-center border-t">
+            <div className="container relative border-x xl:max-w-7xl">
+              <PixelatedLeft
+                className="-z-10 -translate-x-full absolute top-0 left-0 h-full w-auto opacity-50"
+                style={{ mixBlendMode: "screen" }}
+              />
+              <PixelatedRight
+                className="-z-10 absolute top-0 right-0 h-full w-auto translate-x-full opacity-50"
+                style={{ mixBlendMode: "screen" }}
+              />
+              <div className="-top-4 -left-px absolute hidden h-4 w-px bg-border md:block" />
+              <div className="-top-4 -right-px absolute hidden h-4 w-px bg-border md:block" />
+              {tabs.map((tab) => (
+                <TabsContent
+                  key={tab.value}
+                  value={tab.value}
+                >
+                  <Image
+                    src="/email-preview.png"
+                    alt="Zero Email Preview"
+                    width={1920}
+                    height={1080}
+                    className=""
+                  />
+                </TabsContent>
+              ))}
             </div>
-
-            <div className="flex w-full justify-center border-t">
-              <div className="container relative border-x xl:max-w-7xl">
-                <div className="-top-4 -left-px absolute h-4 w-px bg-border" />
-                <div className="-top-4 -right-px absolute h-4 w-px bg-border" />
-                {tabs.map((tab) => (
-                  <TabsContent
-                    key={tab.value}
-                    value={tab.value}
-                  >
-                    <Image
-                      src="/email-preview.png"
-                      alt="Zero Email Preview"
-                      width={1920}
-                      height={1080}
-                      className=""
-                    />
-                  </TabsContent>
-                ))}
-              </div>
-            </div>
-          </Tabs>
-        </div>
+          </div>
+        </Tabs>
       </section>
     </main>
   )
